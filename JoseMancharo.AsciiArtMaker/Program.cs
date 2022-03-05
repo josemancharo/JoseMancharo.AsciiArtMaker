37d987f55ec0203a5e22bb9898d959eca63937cf
isaiahpettingill <isaiah@redpeppersoftware.com>
2022-03-05 13:09:24 -0700

    with code

---
+++ JoseMancharo.AsciiArtMaker/Program.cs
@@ -0,0 +1,41 @@
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Advanced;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.ImageSharp.PixelFormats;
+using System.Text;
+using System.Linq;
+using JoseMancharo.AsciiArtMaker;
+
+var asciiCharacters = new[] { '#', '0', '8', '7', '@', '%', '=', '+', '|', '!', ';', ':', '*', '"', '`', ',', '.', ' ' }.Reverse().ToArray();
+
+Console.WriteLine("input path to file: ");
+var file = Console.ReadLine();
+
+using var image = Image.Load<Rgba32>(file);
+var newHeight = (int)Math.Round(image.Height / (image.Width / 225f));
+image.Mutate(x => x.Resize(225, newHeight));
+
+if (image.Frames.Count > 1)
+{
+    var ascii_frames = new List<string>();
+    for (var i = 0; i<image.Frames.Count; i++)
+    {
+        ascii_frames.Add(image.Frames[i].ToAsciiArt(asciiCharacters));
+    }
+
+    using var outputStream = Console.OpenStandardOutput();
+    var emptyFrame = string.Join('\n', Enumerable.Range(0, newHeight).Select(_ => ""));
+    while (true)
+    {
+        foreach (var frame in ascii_frames)
+        {
+            new MemoryStream(Encoding.UTF8.GetBytes(emptyFrame + frame)).CopyTo(outputStream);
+            Thread.Sleep(10);
+
+        }
+    }
+}
+else
+{
+    Console.WriteLine(image.ToAsciiArt(asciiCharacters));
+}
---
+++ JoseMancharo.AsciiArtMaker/Transformer.cs
@@ -0,0 +1,70 @@
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JoseMancharo.AsciiArtMaker;
+
+internal static class Transformer
+{
+    public static string ToAsciiArt(this Image<Rgba32> image, char[] asciiCharacters)
+    {
+        var builder = new StringBuilder();
+        var isNewLine = false;
+        for (var y = 0; y < image.Height; y++)
+        {
+            for (var x = 0; x < image.Width; x++)
+            {
+                var pixel = image[x, y];
+                var avg = (byte)((pixel.R + pixel.G + pixel.B) / 3);
+                var index = (avg * (asciiCharacters.Length - 1)) / 255;
+                if (!isNewLine)
+                {
+                    builder.Append(asciiCharacters[index]);
+                }
+            }
+            if (!isNewLine)
+            {
+                builder.AppendLine();
+                isNewLine = true;
+            }
+            else
+            {
+                isNewLine = false;
+            }
+        }
+        return builder.ToString();
+    }
+
+    public static string ToAsciiArt(this ImageFrame<Rgba32> image, char[] asciiCharacters)
+    {
+        var builder = new StringBuilder();
+        var isNewLine = false;
+        for (var y = 0; y < image.Height; y++)
+        {
+            for (var x = 0; x < image.Width; x++)
+            {
+                var pixel = image[x, y];
+                var avg = (byte)((pixel.R + pixel.G + pixel.B) / 3);
+                var index = (avg * (asciiCharacters.Length - 1)) / 255;
+                if (!isNewLine)
+                {
+                    builder.Append(asciiCharacters[index]);
+                }
+            }
+            if (!isNewLine)
+            {
+                builder.AppendLine();
+                isNewLine = true;
+            }
+            else
+            {
+                isNewLine = false;
+            }
+        }
+        return builder.ToString();
+    }
+}
